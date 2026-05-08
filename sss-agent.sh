a634bf2d800ef584d5fbdb19179ca73add10c7ae
jscntw <97719249+jscntw@users.noreply.github.com>
2026-05-08 21:22:02 +0800

    Update sss-agent.sh

--- sss-agent.sh
+++ sss-agent.sh
@@ -7,10 +7,10 @@
 #   Github: https://github.com/lidalao/ServerStatus
 #========================================================
 
-SSS_BASE_PATH="/opt/sss"
+SSS_BASE_PATH="/opt/stacks/sss"
 SSS_AGENT_PATH="${SSS_BASE_PATH}/agent"
 SSS_AGENT_SERVICE="/etc/systemd/system/sss-agent.service"
-GITHUB_RAW_URL="https://raw.githubusercontent.com/lidalao/ServerStatus/master"
+GITHUB_RAW_URL="https://raw.githubusercontent.com/jscntw/ServerStatus/master"
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -97,7 +97,7 @@ uninstall_agent() {
 show_menu() {
     echo -e "
     ${green}Server Status监控管理脚本${plain}
-    --- https://github.com/lidalao/sss ---
+    --- https://github.com/jscntw/serverstatus ---
     ${green}1.${plain}  安装监控Agent
     ${green}2.${plain}  卸载Agent
     ${green}0.${plain}  退出脚本
